061e2f6f4e81b0b021a7e0fc8d07b6c6239bcbad
Matthew Richter <mrichter@fullsail.edu>
2012-09-13 21:28:29 -0400

    A little more clean up and minor corrections

--- asdcouch/app/_attachments/main.js
+++ asdcouch/app/_attachments/main.js
@@ -1,50 +1,7 @@
-// Matthew RIchter 
-// ASD - Full Sail University
+// Matthew Richter 
+// ASD 1209 - Full Sail University
 // Project 3
-// July 19, 2012
-
-// 
-//var parseProjectForm = function(data){
-// uses form data here 
-//	console.log(data);
-//};
-
-/*
-sample code from video why jQuery on using JavaScript without library then with library 
-window.addEventListener('DOMCOntentLoaded',function(){
-	document.getElementById('somelink')
-	.addEventListener('click', myFn);
-});
-thie above will only work in browsers that support W3C
-
-jQuery equivilant
-
-$(function(){
-	$('somelink').on('click', myFn);
-});
-*/
-//
-
-//$(document).ready(function(){ - this allows us to wait until the document is fully loaded before we run our code
-// this wraps all of our code
-// the $ = jQuery object name space - so when we use the $ / the jQuery name space is broken down into two main categories
-// utilities methods are all of the methods that are directly on the object itself  example $.trim()  $.merge(); etc... reference the cheat sheet
-// the second category is the factory using jquery as a function $() - when the jquery factory is mentioned or referenced it means $()
-// the primary use of the factory is to find an element within the page then is gives us additional methods that we can call on top of it.
-// we can call global objects like $(document) or $(window) but the more common use is to call a CSS selector expression
-// $('')  allows us to pass a string with a CSS selector expression to search the DOM to find elements
-// example $('#nav li') this will find all li(s) within a the nav divs we can also consolelog this  consolelog( $('#nav li'));
-// when the factory pulls up these elements we can also then we can use the additional methiods jQuery provides on top of it
-// example $('#nav li').fadeOut();  this will automatically loop through all the li(s) and fade them out
-// Important ****  most of the methods return exactly what the factor called - this can be viewed by consoleloggin the factory call and method see below
-// consolelog( $('#nav li').fadeOut() ); - this is important to us becasue we can keep calling methods so we can chain
-// example $('#nav li').fadeOut().fadeIn().animate({fontSize:30},100);
-// Downside to calling the factory is that everytime we call the factory it creates an object so we can create a var
-// example var navlist = $('nav li').fadeOut() etc....  so the next time we want to access the nav li(s) we call var navlist
-// some of the jquery factory methods are not chainable $('#nav').hasClass('myClass') returns a boolean - true or false 
-// the hasClass is a good way to search through you document to fins all the elements that have a certain class - in the example above it would be all the nav(s) that have a certain class 
-
-
+// September 11, 2012
 
 //$("#home").on('pageinit',function(){
 $("#home").live("pageshow", function() {
@@ -60,8 +17,8 @@ $("#home").live("pageshow", function() {
 				$("#dataHolder").append(
 					$("<li>").append(
 							$("<a>")
-								.attr("href", "/asdprojecttwo/_all_docs?include_docs=true&key=" + '"' + item._id + '"' ) 
-								.attr("id", "detail")
+								.attr('href', '/asdprojecttwo/_all_docs?include_docs=true&key=' + '"' + item._id + '"' ) 
+								.attr("id", "project")
 								.text(item.projectName)
 					)	
 				);
