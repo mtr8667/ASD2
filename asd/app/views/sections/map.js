156b782a929639abd58459373198664c0f6409ad
Matthew Richter <mrichter@fullsail.edu>
2012-09-20 13:52:22 -0400

    In an effort to fix the cloudant error I have rebuilt my entire couchDB app from scratch in a new DB asd instead of asdprojecttwo
    Marianne I'm starting to panic !!!!!! I'm still getting the error in
    cloudant

---
+++ asd/app/views/sections/map.js
@@ -0,0 +1,11 @@
+function(doc) {
+	  if (doc._id.substr(0,8) ==="section:") {
+	    emit(doc._id.substr(8), {
+	    	 "_id":			doc._id,
+	    	 "_rev":		doc._rev,
+	    	 "title": 		doc.title,
+	    	 "name":		doc.name,
+	    	 "description": doc.description
+	    });
+	  }
+	};
